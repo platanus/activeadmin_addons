00528c779d97648cf608706524bee2092582edef
juliogarciag <julioggonz@gmail.com>
2014-06-06 10:23:29 -0500

    bool_values addon

--- lib/activeadmin_ui_addons/railtie.rb
+++ lib/activeadmin_ui_addons/railtie.rb
@@ -1,41 +1,7 @@
 module ActiveAdminUIAddons
-  # TODO: Extract together with the bool methods
-  DEFAULT_BOOLEAN_TRUE = '&#x2714;'
-  DEFAULT_BOOLEAN_FALSE = '&#x2717;'
-
   class Railtie < Rails::Railtie
-    initializer "set boolean stuff" do
-
-      ActiveSupport.on_load(:action_controller) do
-        # TODO: Extract bool methods
-        module ::ActiveAdmin
-          module Views
-            class TableFor
-              def bool_column(attribute)
-                column(attribute) do |model|
-                  if model[attribute]
-                    I18n.t('activeadmin.addons.boolean.true', default: ActiveAdminUIAddons::DEFAULT_BOOLEAN_TRUE).html_safe
-                  else
-                    I18n.t('activeadmin.addons.boolean.false', default: ActiveAdminUIAddons::DEFAULT_BOOLEAN_FALSE).html_safe
-                  end
-                end
-              end
-            end
-            class AttributesTable
-              def bool_row(attribute)
-                row(attribute) do |model|
-                  if model[attribute]
-                    I18n.t('activeadmin.addons.boolean.true', default: ActiveAdminUIAddons::DEFAULT_BOOLEAN_TRUE).html_safe
-                  else
-                    I18n.t('activeadmin.addons.boolean.false', default: ActiveAdminUIAddons::DEFAULT_BOOLEAN_FALSE).html_safe
-                  end
-                end
-              end
-            end
-          end
-        end
-      end
-
+    initializer "set boolean values addon" do
+      require_relative './addons/bool_values'
     end
   end
 end
