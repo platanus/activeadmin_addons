11147ad5fcef365f0577b518718d39477d4d741e
Emilio Blanco <emilioeduardob@gmail.com>
2015-03-03 14:01:01 -0300

    Test deploy

--- lib/activeadmin_ui_addons/engine.rb
+++ lib/activeadmin_ui_addons/engine.rb
@@ -1,8 +1,10 @@
 module ActiveAdminUIAddons
   module Rails
     class Engine < ::Rails::Engine
-      initializer "set boolean values addon" do
+      require 'select2-rails'
+      initializer "set boolean values addon" do |app|
         require_relative './addons/bool_values'
+        app.config.assets.precompile +=  %w(select.scss)
       end
     end
   end
