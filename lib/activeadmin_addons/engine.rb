422467edf60ba01e2cf139c5cbdd18bb8e8353f2
Emilio Blanco <emilioeduardob@gmail.com>
2015-04-24 12:01:11 -0400

    feat(Datepicker): Set default datepicker

--- lib/activeadmin_addons/engine.rb
+++ lib/activeadmin_addons/engine.rb
@@ -3,6 +3,7 @@ module ActiveAdminAddons
     class Engine < ::Rails::Engine
       require 'select2-rails'
       initializer "set boolean values addon" do |app|
+        require_relative './set_datepicker'
         require_relative './addons/bool_values'
         app.config.assets.precompile +=  %w(select.scss)
       end
