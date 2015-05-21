a64be3f3cfb790c775c364535a1cfb922cb30423
Emilio Blanco <emilioeduardob@gmail.com>
2015-05-21 09:52:30 -0400

    chore(): Move file to follow directory structure

--- lib/activeadmin_addons/engine.rb
+++ lib/activeadmin_addons/engine.rb
@@ -3,12 +3,12 @@ module ActiveAdminAddons
     class Engine < ::Rails::Engine
       require 'select2-rails'
       initializer "set boolean values addon" do |app|
-        require_relative './set_datepicker'
         require_relative './addons/bool_values'
         require_relative './addons/paperclip_image'
         require_relative './addons/paperclip_attachment'
         require_relative './addons/enum_tag'
         require_relative './support/enumerize_formtastic_support'
+        require_relative './support/set_datepicker'
         app.config.assets.precompile +=  %w(select.scss)
       end
     end
