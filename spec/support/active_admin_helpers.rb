9692d5da63c276394c422a7dc8b51ae34896f2a5
Emilio Blanco <emilioeduardob@gmail.com>
2015-09-25 14:52:36 -0400

    refactor(BoolValues): Improve test with register_show & register_index helpers

--- spec/support/active_admin_helpers.rb
+++ spec/support/active_admin_helpers.rb
@@ -27,6 +27,30 @@ module ActiveAdminHelpers
     end
   end
 
+  def register_index(klass, &block)
+    load_resources do
+      ActiveAdmin.register(klass) do
+        index do
+          instance_eval(&block)
+        end
+      end
+    end
+  end
+
+
+  def register_show(klass, &block)
+    load_resources do
+      ActiveAdmin.register(klass) do
+        show do
+          attributes_table do
+            instance_eval(&block)
+          end
+        end
+      end
+    end
+  end
+
+
   # Sometimes we need to reload the routes within
   # the application to test them out
   def reload_routes!(_show_routes = false)
