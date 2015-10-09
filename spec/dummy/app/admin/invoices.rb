8719b3840f040fad55a08288a5cff614b4284b32
Leandro Segovia <ldlsegovia@gmail.com>
2015-10-09 16:21:36 -0300

    feat(): add invoices page example

--- spec/dummy/app/admin/invoices.rb
+++ spec/dummy/app/admin/invoices.rb
@@ -1,19 +1,38 @@
 ActiveAdmin.register Invoice do
-  permit_params :legal_date, :number, :paid
+  permit_params :legal_date, :number, :paid, :state, :attachment, :photo
 
   index do
     selectable_column
     id_column
+    tag_column :state
     bool_column :paid
+    image_column :photo, style: :thumb
+    attachment_column :attachment
+    column :legal_date
+    number_column :number, as: :currency, unit: "$", separator: ","
     actions
   end
 
   show do
     attributes_table do
       row :id
-      row :legal_date
-      row :number
+      tag_row :state
       bool_row :paid
+      image_row("Mi foto", :photo, style: :big, &:photo)
+      attachment_row("Mi documento", :attachment, truncate: false, &:attachment)
+      row :legal_date
+      number_row("Número", :number, as: :human, &:number)
     end
+  end
+
+  form do |f|
+    f.inputs "Details" do
+      f.input :state
+      f.input :paid
+      f.input :number
+      f.input :attachment
+      f.input :photo
+    end
+    f.actions
   end
 end
