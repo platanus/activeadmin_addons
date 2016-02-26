5e413beb779b7efe77082c85eca4e2beea2935ce
Agustin Feuerhake <agustin@platan.us>
2016-02-26 17:27:40 -0300

    fix(style): to match my dear monkey friend

--- lib/generators/activeadmin_addons/install/install_generator.rb
+++ lib/generators/activeadmin_addons/install/install_generator.rb
@@ -1,13 +1,17 @@
 module ActiveadminAddons
   module Generators
     class InstallGenerator < Rails::Generators::Base
-
       def add_javascripts
-        inject_into_file 'app/assets/javascripts/active_admin.js.coffee', "#= require activeadmin_addons/all\n", after:"#= require active_admin/base\n"
+        file_path = 'app/assets/javascripts/active_admin.js.coffee'
+        line_to_add = "#= require activeadmin_addons/all\n"
+        reference = "#= require active_admin/base\n"
+        inject_into_file file_path, line_to_add, after: reference
       end
 
       def add_stylesheets
-        prepend_file 'app/assets/stylesheets/active_admin.css.scss', "//= require activeadmin_addons/all\n"
+        file_path = 'app/assets/stylesheets/active_admin.css.scss'
+        line_to_add = "//= require activeadmin_addons/all\n"
+        prepend_file file_path, line_to_add
       end
     end
   end
