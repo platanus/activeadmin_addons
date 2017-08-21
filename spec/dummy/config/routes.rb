b8dbf08e1f9bcb2dde14e465a5b6c73b88cea2fe
Leandro Segovia <ldlsegovia@gmail.com>
2017-08-20 21:15:06 -0300

    chore(): redirect / to /admin

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'admin/dashboard#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
