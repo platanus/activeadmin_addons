597f36fc2a47a6e02767fd69fb2732ec9ccb3046
Dhyego Fernando <dhyegofernando@gmail.com>
2017-09-06 18:46:02 -0400

    fix(NestedSelect): do not create virtual attrs methods when it does already exists

--- app/inputs/nested_select_input.rb
+++ app/inputs/nested_select_input.rb
@@ -65,7 +65,7 @@ class NestedSelectInput < ActiveAdminAddons::InputBase
 
   def set_parent_value(level_data)
     parent_attribute = level_data[:parent_attribute]
-    build_virtual_attr(parent_attribute)
+    build_virtual_attr(parent_attribute) unless @object.respond_to?(parent_attribute)
     instance = instance_from_attribute_name(level_data[:attribute])
     if instance && instance.respond_to?(parent_attribute)
       @object.send("#{parent_attribute}=", instance.send(parent_attribute))
@@ -81,7 +81,6 @@ class NestedSelectInput < ActiveAdminAddons::InputBase
   end
 
   def build_virtual_attr(attribute_name)
-    fail "#{attribute_name} is already defined" if @object.respond_to?(attribute_name)
     @object.singleton_class.send(:attr_accessor, attribute_name)
   end
 end
