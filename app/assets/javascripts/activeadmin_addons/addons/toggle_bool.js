488b089f002a74b30843e63ef31b74bc07da5d80
felbalart <felbalart@gmail.com>
2017-11-10 22:44:32 -0300

    style(toggle-bool-js): fix style issues

--- app/assets/javascripts/activeadmin_addons/addons/toggle_bool.js
+++ app/assets/javascripts/activeadmin_addons/addons/toggle_bool.js
@@ -1,15 +1,15 @@
 $(function() {
-  $('.toggle-bool-switch').click(function (e) {
+  $('.toggle-bool-switch').click(function(e) {
     var boolSwitch = $(e.target);
     var model = boolSwitch.data('model');
     var objectId = boolSwitch.data('object_id');
     var field = boolSwitch.data('field');
     var value = boolSwitch.data('value');
-    var otherValue = String(value)!=='true';
+    var otherValue = String(value) !== 'true';
     var switchId = boolSwitch.attr('id');
-    var otherSwitch = $('#' + switchId.substr(0, switchId.lastIndexOf("-") + 1) + otherValue);
+    var otherSwitch = $('#' + switchId.substr(0, switchId.lastIndexOf('-') + 1) + otherValue);
     var url = boolSwitch.data('url');
-    data = { id: objectId };
+    var data = { id: objectId };
     data[model] = {};
     data[model][field] = otherValue;
     $.ajax({
@@ -17,19 +17,19 @@ $(function() {
       data: data,
       dataType: 'json',
       error: function() {
-        var errorMsg = "Error: Update Unsuccessful";
+        var errorMsg = 'Error: Update Unsuccessful';
         alert(errorMsg);
       },
-      success: function(data) {
+      success: function() {
         boolSwitch.addClass('hidden-switch');
         otherSwitch.removeClass('hidden-switch');
-        if(!boolSwitch.hasClass('notify-success')) return;
-          $(function() {
-             var successMsg = 'Update Successful!';
-            setTimeout(alert(successMsg), 500)
-          });
+        if (!boolSwitch.hasClass('notify-success')) return;
+        $(function() {
+          var successMsg = 'Update Successful!';
+          setTimeout(alert(successMsg), 500);
+        });
       },
-      type: 'PATCH'
+      type: 'PATCH',
     });
   });
 });
