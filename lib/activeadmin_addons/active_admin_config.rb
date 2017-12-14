e766940b434a5d5726c56f232b2dddf73e13ac68
Christian Trosclair <xn@users.noreply.github.com>
2017-12-14 11:04:20 -0600

    pass args to original method

--- lib/activeadmin_addons/active_admin_config.rb
+++ lib/activeadmin_addons/active_admin_config.rb
@@ -2,7 +2,7 @@ class ActiveAdmin::Views::Pages::Base
   alias_method :original_build, :build
 
   def build(*args)
-    original_build
+    original_build(args)
     body.set_attribute "data-default-select", ActiveadminAddons.default_select
   end
 end
