16c36757ace540e56d133e1c3218a4d7fde0bb27
Jose Lezama <joseglego@gmail.com>
2018-03-25 21:16:41 -0300

    chore(icons): Include material-icons

--- lib/activeadmin_addons/engine.rb
+++ lib/activeadmin_addons/engine.rb
@@ -11,7 +11,8 @@ module ActiveAdminAddons
         require_rel "support"
         require_rel "addons"
         require_rel "active_admin_config"
-        app.config.assets.precompile += %w(select.scss fileicons/*.png switches/switch_*.png)
+        app.config.assets.precompile += %w(select.scss fileicons/*.png switches/switch_*.png
+                                           material/icons *.svg)
       end
     end
   end
