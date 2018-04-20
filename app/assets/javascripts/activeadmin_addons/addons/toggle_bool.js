557fd00ac2f6b6852cdd5ef03b5b1d7e68f37824
Jose Lezama <joseglego@gmail.com>
2018-04-19 23:31:59 -0300

    feat(toggle-bool): build span tag, with styles and js interactions

--- app/assets/javascripts/activeadmin_addons/addons/toggle_bool.js
+++ app/assets/javascripts/activeadmin_addons/addons/toggle_bool.js
@@ -1,18 +1,18 @@
 $(function() {
   $('.toggle-bool-switch').click(function(e) {
     var boolSwitch = $(e.target);
-    var model = boolSwitch.data('model');
+
     var objectId = boolSwitch.data('object_id');
+    var model = boolSwitch.data('model');
     var field = boolSwitch.data('field');
-    var value = boolSwitch.data('value');
-    var otherValue = String(value) !== 'true';
-    var switchId = boolSwitch.attr('id');
-    var otherSwitch = $('#' + switchId.substr(0, switchId.lastIndexOf('-') + 1) + otherValue);
+
     var url = boolSwitch.data('url');
+    var value = boolSwitch.data('value');
     var successMessage = boolSwitch.data('success_message');
     var data = { id: objectId };
     data[model] = {};
-    data[model][field] = otherValue;
+    data[model][field] = !value;
+
     $.ajax({
       url: url,
       data: data,
@@ -22,11 +22,10 @@ $(function() {
         alert(errorMsg);
       },
       success: function() {
-        boolSwitch.addClass('hidden-switch');
-        otherSwitch.removeClass('hidden-switch');
+        boolSwitch.data('value', !value);
+        boolSwitch.toggleClass('on');
         if (!boolSwitch.hasClass('notify-success')) return;
         $(function() {
-          var successMsg = 'Update Successful!';
           setTimeout(alert(successMessage), 500);
         });
       },
