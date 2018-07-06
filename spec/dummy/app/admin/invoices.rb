4705bdff7f6cd886a8f5dd488e9618e31306d1af
Jose Lezama <joseglego@gmail.com>
2018-07-06 17:00:32 -0400

    chore(spec-dummy): show better structure in invoice

--- spec/dummy/app/admin/invoices.rb
+++ spec/dummy/app/admin/invoices.rb
@@ -23,8 +23,6 @@ ActiveAdmin.register Invoice do
     image_column :photo, style: :thumb
     attachment_column :attachment
     number_column :amount, as: :currency, unit: "$", separator: ","
-    list_column :skills
-    list_column :contact
     toggle_bool_column :active
     actions
   end
