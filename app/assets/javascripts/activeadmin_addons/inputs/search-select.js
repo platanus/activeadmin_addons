73e0c478f057362921e19696d9d4fa408ee486ba
Clément Prod'homme <prodhomme.clement@hotmail.fr>
2020-10-23 12:34:27 +0200

    extract logic to setup only one input with select2
    With this, i can use setupSearchSelectInput() method to refresh my input

--- app/assets/javascripts/activeadmin_addons/inputs/search-select.js
+++ app/assets/javascripts/activeadmin_addons/inputs/search-select.js
@@ -1,74 +1,78 @@
 var initializer = function() {
-  setupSearchSelect(document);
+  setupSearchSelectInputs(document);
 
   $(document).on('has_many_add:after', function(event, container) {
-    setupSearchSelect(container);
+    setupSearchSelectInputs(container);
   });
 
-  function setupSearchSelect(container) {
+  function setupSearchSelectInputs(container) {
     $('.search-select-input, .search-select-filter-input, ajax-filter-input', container).each(function(i, el) {
-      var element = $(el);
-      var url = element.data('url');
-      var fields = element.data('fields');
-      var predicate = element.data('predicate');
-      var displayName = element.data('display-name');
-      var width = element.data('width');
-      var responseRoot = element.data('response-root');
-      var minimumInputLength = element.data('minimum-input-length');
-      var order = element.data('order');
-
-      var selectOptions = {
-        width: width,
-        minimumInputLength: minimumInputLength,
-        placeholder: '',
-        allowClear: true,
-        ajax: {
-          url: url,
-          dataType: 'json',
-          delay: 250,
-          cache: true,
-          data: function(params) {
-            var textQuery = { m: 'or' };
-            fields.forEach(function(field) {
-              if (field == 'id') {
-                textQuery[field + '_eq'] = params.term;
-              } else {
-                textQuery[field + '_' + predicate] = params.term;
-              }
-            });
-
-            var query = {
-              order: order,
-              q: {
-                groupings: [textQuery],
-                combinator: 'and',
-              },
-            };
-
-            return query;
-          },
-          processResults: function(data) {
-            if (data.constructor == Object) {
-              data = data[responseRoot];
-            }
-
-            return {
-              results: jQuery.map(data, function(resource) {
-                if (!resource[displayName]) {
-                  resource[displayName] = 'No display name for id #' + resource.id.toString();
-                }
-                return {
-                  id: resource.id,
-                  text: resource[displayName].toString(),
-                };
-              }),
-            };
-          },
-        },
-      };
-
-      $(el).select2(selectOptions);
+      setupSearchSelectInput(el);
     });
+  }
+
+  function setupSearchSelectInput(el) {
+    var element = $(el);
+    var url = element.data('url');
+    var fields = element.data('fields');
+    var predicate = element.data('predicate');
+    var displayName = element.data('display-name');
+    var width = element.data('width');
+    var responseRoot = element.data('response-root');
+    var minimumInputLength = element.data('minimum-input-length');
+    var order = element.data('order');
+
+    var selectOptions = {
+      width: width,
+      minimumInputLength: minimumInputLength,
+      placeholder: '',
+      allowClear: true,
+      ajax: {
+        url: url,
+        dataType: 'json',
+        delay: 250,
+        cache: true,
+        data: function(params) {
+          var textQuery = { m: 'or' };
+          fields.forEach(function(field) {
+            if (field == 'id') {
+              textQuery[field + '_eq'] = params.term;
+            } else {
+              textQuery[field + '_' + predicate] = params.term;
+            }
+          });
+
+          var query = {
+            order: order,
+            q: {
+              groupings: [textQuery],
+              combinator: 'and',
+            },
+          };
+
+          return query;
+        },
+        processResults: function(data) {
+          if (data.constructor == Object) {
+            data = data[responseRoot];
+          }
+
+          return {
+            results: jQuery.map(data, function(resource) {
+              if (!resource[displayName]) {
+                resource[displayName] = 'No display name for id #' + resource.id.toString();
+              }
+              return {
+                id: resource.id,
+                text: resource[displayName].toString(),
+              };
+            }),
+          };
+        },
+      },
+    };
+
+    $(el).select2(selectOptions);
   }
 };
 
