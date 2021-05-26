f70b2833a623e6d8f6c743fbd8561a3e1433672a
Leandro Segovia <ldlsegovia@gmail.com>
2021-05-26 10:30:58 -0300

    fix(webpack): duplicated jquery instance

--- spec/dummy/config/webpack/environment.js
+++ spec/dummy/config/webpack/environment.js
@@ -1,5 +1,6 @@
 const { environment } = require('@rails/webpacker')
-const jquery = require('./plugins/jquery')
-
-environment.plugins.prepend('jquery', jquery)
-module.exports = environment
+environment.config.set('externals', {
+    $: 'jquery',
+    jquery: 'jQuery',
+});
+module.exports = environment;
