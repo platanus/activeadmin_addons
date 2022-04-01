e13e573989f161734963b36ecc4cf6c0dd8a5b58
Gabriel Lyon <glyon1@uc.cl>
2022-04-01 18:07:13 -0300

    feat(shrine-initializer): define basic initializer

---
+++ spec/dummy/config/initializers/shrine.rb
@@ -0,0 +1,22 @@
+require 'shrine'
+
+if Rails.env.development?
+  require 'shrine/storage/file_system'
+
+  Shrine.storages = {
+    cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'),
+    store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads')
+  }
+else
+  require 'shrine/storage/memory'
+
+  Shrine.storages = {
+    cache: Shrine::Storage::Memory.new,
+    store: Shrine::Storage::Memory.new
+  }
+end
+
+Shrine.plugin :activerecord
+Shrine.plugin :cached_attachment_data
+Shrine.plugin :restore_cached_data
+Shrine.plugin :derivatives
