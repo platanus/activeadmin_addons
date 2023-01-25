a8a12f2ee1faf31d7051809b82bffbcef490c46a
Guillermo Moreno <gmq@guillermomoreno.cl>
2023-01-25 10:31:52 -0300

    chore(): remove remaining select2 references

--- spec/dummy/config/initializers/activeadmin_addons.rb
+++ spec/dummy/config/initializers/activeadmin_addons.rb
@@ -1,6 +1,6 @@
 ActiveadminAddons.setup do |config|
   # Change to "default" if you want to use ActiveAdmin's default select control.
-  # config.default_select = "select2"
+  # config.default_select = "slim-select"
 
   # Set default options for DateTimePickerInput. The options you can provide are the same as in
   # xdan's datetimepicker library (https://github.com/xdan/datetimepicker/tree/2.5.4). Yo need to
