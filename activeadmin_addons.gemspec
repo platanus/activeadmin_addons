484588fb8c9019da3e5bcf6bb3031a33f3df2603
Guillermo Moreno <gmq@guillermomoreno.cl>
2023-03-14 12:39:54 -0300

    feat(): remove sass dependencies

--- activeadmin_addons.gemspec
+++ activeadmin_addons.gemspec
@@ -22,8 +22,6 @@ Gem::Specification.new do |s|
   s.add_dependency "railties"
   s.add_dependency "redcarpet"
   s.add_dependency "require_all"
-  s.add_dependency "sassc"
-  s.add_dependency "sassc-rails"
   s.add_dependency "xdan-datetimepicker-rails", "~> 2.5.1"
 
   s.add_development_dependency "aasm"
--- lib/activeadmin_addons/engine.rb
+++ lib/activeadmin_addons/engine.rb
@@ -1,8 +1,6 @@
 module ActiveAdminAddons
   module Rails
     class Engine < ::Rails::Engine
-      require 'sassc'
-      require 'sassc-rails'
       require "xdan-datetimepicker-rails"
       require "require_all"
       require "active_material"
@@ -11,8 +9,6 @@ module ActiveAdminAddons
         require_rel "support"
         require_rel "addons"
         require_rel "active_admin_config"
-        app.config.assets.precompile += %w(select.scss fileicons/*.png switches/switch_*.png
-                                           material/icons *.svg)
       end
     end
   end
