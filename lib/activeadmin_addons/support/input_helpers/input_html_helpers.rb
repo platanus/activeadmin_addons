6fe25c5b53de361481d47f8fa4aba1f779107931
Diego Fernández <difernandez@uc.cl>
2023-04-14 11:49:44 -0400

    refactor(input-base): move methods to input_html_helper

--- lib/activeadmin_addons/support/input_helpers/input_html_helpers.rb
+++ lib/activeadmin_addons/support/input_helpers/input_html_helpers.rb
@@ -40,5 +40,47 @@ module ActiveAdminAddons
         value: value
       )
     end
+
+    def to_html
+      load_input_class
+      load_control_attributes
+      render_custom_input
+      if parts.any?
+        input_wrapping { parts_to_html }
+      else
+        super
+      end
+    end
+
+    def input_html_options
+      # maxwidth and size are added by Formtastic::Inputs::StringInput
+      # but according to the HTML standard these are not valid attributes
+      # on the inputs provided by this module
+      super.except(:maxlength, :size).merge(control_attributes)
+    end
+
+    def load_input_class
+      load_class(self.class.to_s.underscore.dasherize)
+    end
+
+    def load_control_attributes
+      # Override on child classes if needed
+    end
+
+    def render_custom_input
+      # Override on child classes if needed
+    end
+
+    def parts_to_html
+      parts.flatten.join("\n").html_safe # rubocop:disable Rails/OutputSafety
+    end
+
+    def concat(part)
+      parts << part
+    end
+
+    def parts
+      @parts ||= []
+    end
   end
 end
