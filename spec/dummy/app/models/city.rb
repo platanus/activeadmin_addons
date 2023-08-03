c913697a8b9736fe0db29cc1378d269bd21c2d4b
Diego Fernández <difernandez@uc.cl>
2023-08-03 12:33:10 -0400

    test(dummy-app): add ransackable attrs and associations
    Now required in AA v3 / Ransack v4

--- spec/dummy/app/models/city.rb
+++ spec/dummy/app/models/city.rb
@@ -1,3 +1,13 @@
 class City < ActiveRecord::Base
   belongs_to :region
+  has_many :departments_cities, dependent: :destroy
+  has_many :departments, through: :departments_cities
+
+  def self.ransackable_attributes(_auth_object = nil)
+    ["created_at", "id", "information", "name", "region_id", "updated_at"]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    ["region", "deparments_cities", "departments"]
+  end
 end
